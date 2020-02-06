89a0aa1fd1fcff6845a68c4e6b06c337b9757e17
Lövlie <fredriklovlie@gmail.com>
2020-02-06 12:10:47 +0100

    Fire Falling Down

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -4,15 +4,103 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum FireState
     {
-        
+        WADDLE_IN,
+        FALL_DOWN,
+        END_STATE
     }
 
-    // Update is called once per frame
+    [SerializeField] private List<Transform> WaypointList = new List<Transform>();
+
+    [SerializeField] private float GoalDistanceThreshold = 0.1f;
+    [SerializeField] private float MovementSpeed = 1.0f;
+    [SerializeField] private float RotationSpeed = 180.0f;
+
+    private float Health = 1.0f;
+
+    private FireState CurrentState = FireState.WADDLE_IN;
+
+    private Transform CurrentWaypoint = null;
+
+    void Start()
+    {
+        CurrentWaypoint = WaypointList[0];
+    }
+
     void Update()
     {
-        
+        switch(CurrentState)
+        {
+            case FireState.WADDLE_IN:
+                MoveToWaypoint(CurrentWaypoint);
+                if (IsGoalReached(CurrentWaypoint))
+                {
+                    StartCoroutine(FallDown());
+                    NextState();
+                }
+                break;
+           case FireState.END_STATE:
+                break;
+        }
+        transform.localScale = new Vector3(Health, Health, Health);
+    }
+
+    private void MoveToWaypoint(Transform pWaypoint)
+    {
+        if (pWaypoint == null)
+        {
+            Debug.LogWarning("Waypoint is not assigned");
+            return;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, pWaypoint.position, MovementSpeed * Time.deltaTime);
+    }
+
+    private bool IsGoalReached(Transform pWaypoint)
+    {
+        if(Vector3.Distance(pWaypoint.position, transform.position) <= GoalDistanceThreshold)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void NextState()
+    {
+        CurrentState++;
+        if (WaypointList.Count > 1)
+        {
+            WaypointList.RemoveAt(0);
+            CurrentWaypoint = WaypointList[0];
+        }
+    }
+
+    private IEnumerator FallDown()
+    {
+
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        eulerRotation.z += 90;
+        Quaternion rotationGoal = transform.rotation;
+        rotationGoal.eulerAngles = eulerRotation;
+
+        Quaternion originalRotation = transform.rotation;
+
+
+        while(Vector3.Distance(transform.rotation.eulerAngles, rotationGoal.eulerAngles) >= 1.0f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationGoal, RotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+        Health -= 0.5f;
+        yield return new WaitForSeconds(1.0f);
+        while (Vector3.Distance(transform.rotation.eulerAngles, originalRotation.eulerAngles) >= 1.0f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, originalRotation, RotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        NextState();
+        yield return null;
+
     }
 }
